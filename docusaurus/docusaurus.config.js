2284661c4aa1f39d4a4a091e6c2db86c8a221e13
paulina-tymoszuk <paulina.tymoszuk.contact@gmail.com>
2025-04-29 13:00:16 +0200

    Update docusaurus.config.js

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   url: 'https://doqario.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/doqario-docs/docusaurus/',
+  baseUrl: '/doqario-docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
