a72ec7a03b2ab476f2ee96325a4345d11d6b9e40
PaulinaTymoszuk <p.tymoszuk@op.pl>
2025-04-29 11:04:19 +0200

    add search bar

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: '',
+  url: 'https://paulinatymoszuk.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -137,6 +137,8 @@ const config = {
         darkTheme: prismThemes.dracula,
       },
     }),
+
+    plugins: [require.resolve('docusaurus-lunr-search')],
 };
 
 export default config;
